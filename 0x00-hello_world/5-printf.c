47b98c1cad6afbc2150625fd6868a0bddf16c3bc
ghost38143 <ghost38143@gmail.com>
2023-09-09 17:48:16 -0700

    using printf function to display

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using the print function to display
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

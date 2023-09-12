c5584e028fdb7e29212b7e005ac7ab4898a22577
ghost38143 <ghost38143@gmail.com>
2023-09-12 08:28:47 -0700

    writing both lower case and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-        char c = 'a';
+       	char c = 'a';
 
-        while (c <= 'z')
-        {
+       	while (c <= 'z')
+	{
 		putchar (c);
 		c++;
 	}
-	c = 'A';
 
+	c = 'A';
 	while (c <= 'Z')
 	{
 		putchar(c);

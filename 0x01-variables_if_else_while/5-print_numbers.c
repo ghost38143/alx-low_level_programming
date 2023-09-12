d1f0dafdf43aab8b7716cffe4d467a3fc9c908f5
ghost38143 <ghost38143@gmail.com>
2023-09-12 09:35:09 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,14 @@
  * Return; always (success)
  */
 int main(void)
+
 {
+
 	int i;
 
 	for (i = 0;i <= 9;i++)
-	{
-		printf("%d", i);
-		printf("\n");
-	}
+	printf("%d", i);
+	printf("\n");
+
 	return (0);
 }

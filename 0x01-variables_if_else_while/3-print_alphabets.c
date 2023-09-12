49e6bb2d30c49d86065808a7a14419667b0e30ab
ghost38143 <ghost38143@gmail.com>
2023-09-12 08:09:44 -0700

    writing both lower case and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - pinting the alphabet using putchar
+ * Return: always  (success)
+ */
+
+int main(void)
+{
+        char c = 'a';
+
+        while (c <= 'z')
+        {
+                putchar(c);
+                c++;
+        }
+
+	c = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar ('\n');
+
+	return(0);
+}

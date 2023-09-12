c733aa68ed873f325f1f5422435793a4ae526a57
ghost38143 <ghost38143@gmail.com>
2023-09-12 09:38:09 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - writing all the single digits
- * Return; always (success)
+ * Return: always (success)
  */
 int main(void)
 
@@ -10,8 +10,8 @@ int main(void)
 	int i;
 
 	for (i = 0;i <= 9;i++)
-	printf("%d", i);
-	printf("\n");
+	printf("%d", i); 
+	printf("\n"); 
 
 	return (0);
 }

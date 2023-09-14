338d87b3b5a2670e36ae225ad9b802f2a206dc33
ghost38143 <ghost38143@gmail.com>
2023-09-13 18:58:37 -0700

    a 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,12 @@ void times_table(void)
 			z = x * y;
 			if (z > 9)
 			{
-				u = z% 10;
+				u = z % 10;
 			       d = (z - u) / 10;
-		      		 _putchar (44);
-			   	 _putchar (32);
-				 _putchar (d + '0');
-				 _putchar (u + '0');
+	      		 _putchar (44);
+		 	 _putchar (32);
+			 _putchar (d + '0');
+			 _putchar (u + '0');
 			}
 			else
 			{

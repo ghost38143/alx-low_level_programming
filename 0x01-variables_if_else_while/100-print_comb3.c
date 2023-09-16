b437d6238de6fb53341714436cd63f3f0d44c3f4
ghost38143 <ghost38143@gmail.com>
2023-09-15 17:28:49 -0700

    printing combo numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,15 @@ int main(void)
 
 	for (x = 0; x <= 8; x++)
 	{
-		for (y = 1; y <= 9; y++)
+		for (y = x + 1; y <= 9; y++)
 		{
 			putchar (x + '0');
 			putchar (y + '0');
-			putchar (',');
-			putchar (' ');
+			if (x != 8 || y != 9)
+			{
+				putchar (',');
+				putchar (' ');
+			}
 		}
 	}
 	putchar ('\n');

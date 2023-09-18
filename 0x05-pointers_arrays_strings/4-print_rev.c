83d62c261152928886af2d80147accb996939620
ghost38143 <ghost38143@gmail.com>
2023-09-18 16:34:08 -0700

    printing a string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - printing a reverse string
+ * @s: the string to print
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (i = length; i > 0; i--)
+	{
+		_putchar (*s);
+		s--;
+	}
+	_putchar ('\n');
+}

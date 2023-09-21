779a36abc0ac2b04087fda4ff5cfbd7467271ba4
ghost38143 <ghost38143@gmail.com>
2023-09-21 14:08:37 -0700

    number of times a word can be linked

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - linking strings
+ * @dest: the string to be copied to
+ * @src: the string to append
+ * @n: maximum number of charcters to copy from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = *src;
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}

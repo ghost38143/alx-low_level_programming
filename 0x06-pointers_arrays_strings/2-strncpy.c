154524b6942cfd1798fd290d0615c0272e4b7201
ghost38143 <ghost38143@gmail.com>
2023-09-21 14:35:57 -0700

    coping strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,11 +4,13 @@
  * @dest: destination string
  * @src: source string
  * @n: number of values to be copied
- * return: dest
+ * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	while (i < n)

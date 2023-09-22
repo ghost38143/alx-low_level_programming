8268020f354ed4974d49e782aec3e558b4e2723b
ghost38143 <ghost38143@gmail.com>
2023-09-22 16:50:58 -0700

    printing numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - printing an integer
+ * @n: number to be printed
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = - n;
+	}
+	if (n < 10)
+	{
+		_putchar (n + '0');
+	}
+	else
+	{
+		print_number(n / 10);
+		_putchar ((n % 10) + '0');
+
+	}
+}

7ce15ec4ee3ef4e5d8344d30b20f5698afd5e58b
ghost38143 <ghost38143@gmail.com>
2023-10-01 14:08:59 -0700

    checking if a string is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@ int strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 + strlen_recursion (s + 1));
+		return (1 + strlen_recursion(s + 1));
 }
 /**
  * comparator - compares each charcter of the string
@@ -24,7 +24,7 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + comparator (s, n1 + 1, n2 - 1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
@@ -39,5 +39,5 @@ int is_palindrome(char *s)
 	{
 		return (1);
 	}
-	return (comparator(s, 0, strlen_recursion (s) - 1));
+	return (comparator(s, 0, strlen_recursion(s) - 1));
 }

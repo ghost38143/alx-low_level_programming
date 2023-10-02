e5260a2478d985d37574f4e107e0194ab5593fd7
ghost38143 <ghost38143@gmail.com>
2023-10-02 00:29:58 -0700

    printing the value of multiplication through arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print the result of your multiplication
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: value
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int value = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			value *= atoi(argv[i]);
+		}
+		printf("%d\n", value);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}

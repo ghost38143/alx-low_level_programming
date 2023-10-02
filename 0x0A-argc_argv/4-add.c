d07a4678e873921bace23e878827c8adf94f4382
ghost38143 <ghost38143@gmail.com>
2023-10-02 01:08:51 -0700

    adding values

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - a function that add two numbers in an argument
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: value
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0 && argv[i][0] != '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+	return (0);
+}

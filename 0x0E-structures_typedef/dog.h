4b61eb71fae2393cc87dd7fe0886fe2103701994
ghost38143 <ghost38143@gmail.com>
2023-10-10 13:29:00 -0700

    defining a new structure for the main function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - represents the name age and owner
+ * @name: pointer to the name of the dog
+ * @age: the age of the dog
+ * @owner: pointer to the owner of the dog
+ */
 struct dog
 {
 	char *name;

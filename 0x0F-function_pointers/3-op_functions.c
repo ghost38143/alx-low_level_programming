6cdd48423fec7e4ac4c95a37680c2cbfa7de254e
ghost38143 <ghost38143@gmail.com>
2023-10-12 17:25:44 -0700

    this is ia a calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - adding two parameters
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substractig two parameters
+ * @a: first para
+ * @b: second para
+ * Return: the value
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplying two parameters
+ * @a: first
+ * @b: second
+ * Return: the value
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - dividing two parameters
+ * @a: first
+ * @b: second
+ * Return: the vaalue
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - finding the reainder
+ * @a: first
+ * @b: second
+ * Return: the value
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

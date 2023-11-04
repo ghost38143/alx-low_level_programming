8bae895724a2510917946d64c42bf15998d93ac7
ghost38143 <ghost38143@gmail.com>
2023-11-04 14:11:56 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 int _putchar (char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

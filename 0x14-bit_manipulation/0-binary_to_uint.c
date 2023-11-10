70a3eda491ff4ca1ef6aaf41870cc5b9f1476159
ghost38143 <ghost38143@gmail.com>
2023-11-10 13:08:16 -0800

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binarty_to_uint - function converts a binary number to unsigned int
+ * binary_to_uint - function converts a binary number to unsigned int
  * @b: pointer to a string containing a binary number
  * Return: unsigned int with decimal value of biunary number or 0 if error
  */
